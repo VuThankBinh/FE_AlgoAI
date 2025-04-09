4e9d55edc566a3ede92caa07e8c67ccf5d70eccb
VuThankBinh <binhthanh.vubc269@gmail.com>
2025-04-10 01:08:51 +0700

    hihi

---
+++ app/src/main/java/com/example/nckh/baitai_lythuyet.java
@@ -0,0 +1,14 @@
+package com.example.nckh;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class baitai_lythuyet extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_baitai_lythuyet);
+    }
+}
